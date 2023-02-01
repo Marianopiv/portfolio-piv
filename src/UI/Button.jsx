14e6e91430eb0df0fd534f4e82097dd1ee6cf3b9
Marianopiv <marianopividori93@gmail.com>
2023-02-01 20:40:15 -0300

    agrego cosas

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({action,text,className}) => {
   return (
-    <div onClick={action} className={`bg-blue-700 text-sm font-bold text-white uppercase font-sans hover:bg-blue-800 hover:cursor-pointer rounded-md p-3 ${className}`}>{text}</div>
+    <div onClick={action} className={`bg-blue-900 text-sm font-bold text-white uppercase font-sans hover:bg-blue-800 hover:cursor-pointer rounded-md p-3 ${className}`}>{text}</div>
   )
 }
 
